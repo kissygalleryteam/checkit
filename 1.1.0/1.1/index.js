9f759500ad03406a983cb3843f40fec13679f391
kissyteam <tiehang.lth@taobao.com>
2015-04-14 09:46:18 +0800

    sync daily version 1.1.0

---
+++ 1.1.0/1.1/index.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Form Auth For Kissy
+ * @author zhangting@taobao.com<zhangting@taobao.com>
+ * @module auth
+ **/
+KISSY.add(function (S, CheckIt) {
+    return CheckIt;
+}, {requires:['./lib/base']});
