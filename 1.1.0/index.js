ec43f7cf8cfe3c96d0ce8c74efcd6ca9fe55cb72
kissyteam <tiehang.lth@taobao.com>
2015-04-14 11:15:45 +0800

    sync daily branch 1.1.1

--- 1.1.0/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview Form Auth For Kissy
- * @author zhangting@taobao.com<zhangting@taobao.com>
- * @module auth
- **/
-KISSY.add(function (S, CheckIt) {
-    return CheckIt;
-}, {requires:['./lib/base']});
